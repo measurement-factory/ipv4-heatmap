07bd142e816747a6c8dd44eaff3139454499eab2
Duane Wessels <wessels@measurement-factory.com>
2007-08-30 05:25:18 +0000

    indented

--- annotate.c
+++ annotate.c
@@ -42,147 +42,146 @@ unsigned int allones = ~0;
 #endif
 
 /*
- * Find the "bounding box" for the CIDR prefix starting
- * at 'first' and having "slash" value of 'slash'
- *
- * For square areas this is pretty easy.  We know how to find the
- * point diagonally opposite the first value (add 1010..1010).
- * Its a little harder for rectangular areas, so I cheat a little
- * and divide it into the two smaller squares.
+ * Find the "bounding box" for the CIDR prefix starting at 'first' and having
+ * "slash" value of 'slash'
+ * 
+ * For square areas this is pretty easy.  We know how to find the point diagonally
+ * opposite the first value (add 1010..1010). Its a little harder for
+ * rectangular areas, so I cheat a little and divide it into the two smaller
+ * squares.
  */
 void
 bounding_box(unsigned int first, int slash, int *xmin, int *ymin, int *xmax, int *ymax)
 {
-	unsigned int diag = 0xAAAAAAAA;
-	int x1, y1, x2, y2;
+    unsigned int diag = 0xAAAAAAAA;
+    int x1, y1, x2, y2;
 
-	/*
-	 * find the point diagonally opposite the first point
-	 */
-	diag = 0xAAAAAAAA;
-	if (0 == (slash & 1)) {
-		/* square */
-		diag >>= slash;
-		hil_xy_from_s(first >> 8, 12, &x1, &y1);
-		hil_xy_from_s((first+diag) >> 8, 12, &x2, &y2);
-		*xmin = MIN(x1,x2);
-		*ymin = MIN(y1,y2);
-		*xmax = MAX(x1,x2);
-		*ymax = MAX(y1,y2);
-	} else {
-		/* rectangle */
-		int x3, y3, x4, y4;
-		slash += 1;
-		diag >>= slash;
-		hil_xy_from_s(first >> 8, 12, &x1, &y1);
-		hil_xy_from_s((first+diag) >> 8, 12, &x2, &y2);
-		first += (1 << (32-slash));
-		hil_xy_from_s((first) >> 8, 12, &x3, &y3);
-		hil_xy_from_s((first+diag) >> 8, 12, &x4, &y4);
-		*xmin = MIN(MIN(x1,x2),MIN(x3,x4));
-		*ymin = MIN(MIN(y1,y2),MIN(y3,y4));
-		*xmax = MAX(MAX(x1,x2),MAX(x3,x4));
-		*ymax = MAX(MAX(y1,y2),MAX(y3,y4));
-	}
+    /*
+     * find the point diagonally opposite the first point
+     */
+    diag = 0xAAAAAAAA;
+    if (0 == (slash & 1)) {
+	/* square */
+	diag >>= slash;
+	hil_xy_from_s(first >> 8, 12, &x1, &y1);
+	hil_xy_from_s((first + diag) >> 8, 12, &x2, &y2);
+	*xmin = MIN(x1, x2);
+	*ymin = MIN(y1, y2);
+	*xmax = MAX(x1, x2);
+	*ymax = MAX(y1, y2);
+    } else {
+	/* rectangle */
+	int x3, y3, x4, y4;
+	slash += 1;
+	diag >>= slash;
+	hil_xy_from_s(first >> 8, 12, &x1, &y1);
+	hil_xy_from_s((first + diag) >> 8, 12, &x2, &y2);
+	first += (1 << (32 - slash));
+	hil_xy_from_s((first) >> 8, 12, &x3, &y3);
+	hil_xy_from_s((first + diag) >> 8, 12, &x4, &y4);
+	*xmin = MIN(MIN(x1, x2), MIN(x3, x4));
+	*ymin = MIN(MIN(y1, y2), MIN(y3, y4));
+	*xmax = MAX(MAX(x1, x2), MAX(x3, x4));
+	*ymax = MAX(MAX(y1, y2), MAX(y3, y4));
+    }
 }
 
 void
 annotate_text(const char *text, int xmin, int ymin, int xmax, int ymax)
 {
-	int fi;
-	for (fi = 0; fi < NFONTS; fi++) {
-		int rendered_width = fonts[fi]->w * strlen(text);
-		if (rendered_width > (xmax-xmin))
-			continue;
-		gdImageString(image, fonts[fi],
-			((xmin+xmax) / 2) - (strlen(text) * fonts[fi]->w / 2),
-			((ymin+ymax) / 2) - (fonts[fi]->h / 2),
-			(char *) text, fontColor);
-		break;
-	}
+    int fi;
+    for (fi = 0; fi < NFONTS; fi++) {
+	int rendered_width = fonts[fi]->w * strlen(text);
+	if (rendered_width > (xmax - xmin))
+	    continue;
+	gdImageString(image, fonts[fi],
+	    ((xmin + xmax) / 2) - (strlen(text) * fonts[fi]->w / 2),
+	    ((ymin + ymax) / 2) - (fonts[fi]->h / 2),
+	    (char *)text, fontColor);
+	break;
+    }
 }
 
 void
 annotate_cidr(const char *cidr, const char *label)
 {
-	char cidr_copy[24];
-	char *t;
-	int slash;
-	unsigned int first;
-	unsigned int last;
-	int xmin, ymin, xmax, ymax;
-	gdPoint points[4];
-	strncpy(cidr_copy, cidr, 24);
-	t = strchr(cidr_copy, '/');
-	if (NULL == t) {
-		fprintf(stderr, "WARNING: missing / on CIDR '%s'\n", cidr_copy);
-		return;
-	}
-	*t++ = '\0';
-	slash = atoi(t);
-	if (1 != inet_pton(AF_INET, cidr_copy, &first)) {
-		fprintf(stderr, "WARNING: inet_pton failed on '%s'\n", cidr_copy);
-		return;
-	}
-	first = ntohl(first);
-	last = first | (allones >> slash);
-	bounding_box(first, slash, &xmin, &ymin, &xmax, &ymax);
-	if (debug) {
-		char fstr[24];
-		char lstr[24];
-		unsigned int tmp;
-		tmp = htonl(first);
-		inet_ntop(AF_INET, &tmp, fstr, 24);
-		tmp = htonl(last);
-		inet_ntop(AF_INET, &tmp, lstr, 24);
-		fprintf(stderr, "cidr=%s"
-			", first=%s, last=%s, last-first=%u"
-			", bbox=%d,%d to %d,%d"
-			"\n", 
-			cidr, fstr, lstr, last-first, 
-			xmin, ymin, xmax, ymax);
-	}
-	if (fontColor < 0)
-		fontColor = gdImageColorAllocateAlpha(image, 255, 255, 255, 64);
-	points[0].x = xmin;
-	points[1].x = xmax;
-	points[2].x = xmax;
-	points[3].x = xmin;
-	points[0].y = ymin;
-	points[1].y = ymin;
-	points[2].y = ymax;
-	points[3].y = ymax;
-	gdImagePolygon(image, points, 4, fontColor);
-	annotate_text(label, xmin, ymin, xmax, ymax);
+    char cidr_copy[24];
+    char *t;
+    int slash;
+    unsigned int first;
+    unsigned int last;
+    int xmin, ymin, xmax, ymax;
+    gdPoint points[4];
+    strncpy(cidr_copy, cidr, 24);
+    t = strchr(cidr_copy, '/');
+    if (NULL == t) {
+	fprintf(stderr, "WARNING: missing / on CIDR '%s'\n", cidr_copy);
+	return;
+    }
+    *t++ = '\0';
+    slash = atoi(t);
+    if (1 != inet_pton(AF_INET, cidr_copy, &first)) {
+	fprintf(stderr, "WARNING: inet_pton failed on '%s'\n", cidr_copy);
+	return;
+    }
+    first = ntohl(first);
+    last = first | (allones >> slash);
+    bounding_box(first, slash, &xmin, &ymin, &xmax, &ymax);
+    if (debug) {
+	char fstr[24];
+	char lstr[24];
+	unsigned int tmp;
+	tmp = htonl(first);
+	inet_ntop(AF_INET, &tmp, fstr, 24);
+	tmp = htonl(last);
+	inet_ntop(AF_INET, &tmp, lstr, 24);
+	fprintf(stderr, "cidr=%s"
+	    ", first=%s, last=%s, last-first=%u"
+	    ", bbox=%d,%d to %d,%d"
+	    "\n",
+	    cidr, fstr, lstr, last - first,
+	    xmin, ymin, xmax, ymax);
+    }
+    if (fontColor < 0)
+	fontColor = gdImageColorAllocateAlpha(image, 255, 255, 255, 64);
+    points[0].x = xmin;
+    points[1].x = xmax;
+    points[2].x = xmax;
+    points[3].x = xmin;
+    points[0].y = ymin;
+    points[1].y = ymin;
+    points[2].y = ymax;
+    points[3].y = ymax;
+    gdImagePolygon(image, points, 4, fontColor);
+    annotate_text(label, xmin, ymin, xmax, ymax);
 }
 
 /*
- * Input is a text file with TAB-separated fields
- * First field is a CIDR address
+ * Input is a text file with TAB-separated fields First field is a CIDR address
  * Second field is a text label
  */
 void
 annotate_file(const char *fn)
 {
-	char buf[512];
-	FILE *fp = fopen(fn, "r");
-	if (NULL == fp)
-		err(1, fn);
-	fonts[4] = gdFontGetTiny();
-	fonts[3] = gdFontGetSmall();
-	fonts[2] = gdFontGetMediumBold();
-	fonts[1] = gdFontGetLarge();
-	fonts[0] = gdFontGetGiant();
-	while (NULL != fgets(buf, 512, fp)) {
-		char *cidr;
-		char *label;
-	 	cidr = strtok(buf, "\t");
-		if (NULL == cidr)
-			continue;
-	 	label = strtok(NULL, "\t\r\n");
-		if (NULL == label)
-			continue;
-		annotate_cidr(cidr, label);
-	}
-	fclose(fp);
+    char buf[512];
+    FILE *fp = fopen(fn, "r");
+    if (NULL == fp)
+	err(1, fn);
+    fonts[4] = gdFontGetTiny();
+    fonts[3] = gdFontGetSmall();
+    fonts[2] = gdFontGetMediumBold();
+    fonts[1] = gdFontGetLarge();
+    fonts[0] = gdFontGetGiant();
+    while (NULL != fgets(buf, 512, fp)) {
+	char *cidr;
+	char *label;
+	cidr = strtok(buf, "\t");
+	if (NULL == cidr)
+	    continue;
+	label = strtok(NULL, "\t\r\n");
+	if (NULL == label)
+	    continue;
+	annotate_cidr(cidr, label);
+    }
+    fclose(fp);
 }
