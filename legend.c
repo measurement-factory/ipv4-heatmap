a172549ec686c6a308e6a6d42ca8578895ce8ea0
Duane Wessels <wessels@measurement-factory.com>
2007-08-31 23:48:45 +0000

    too much work to get the sample boxes in the legend just right

--- legend.c
+++ legend.c
@@ -11,22 +11,19 @@
 
 #include <gd.h>
 
+#define MAX(a,b) (a>b?a:b)
+
 struct bb {
     int xmin, ymin, xmax, ymax;
 };
+#define BOX_WIDTH(b) (b.xmax-b.xmin)
+#define BOX_HEIGHT(b) (b.ymax-b.ymin)
+#define BOX_CTR_X(b) ((b.xmax+b.xmin)/2)
+#define BOX_CTR_Y(b) ((b.ymax+b.ymin)/2)
 
 struct bb legend_bb = {4096, 0, 5120, 4096};
 struct bb legend_title_bb = {4224, 128, 4992, 512};
 struct bb legend_legend_bb = {4224, 768, 4352, 1024};
-
-char *sample_cidr[] = {
-    "0.0.0.0/8",
-    "0.0.0.0/12",
-    "0.0.0.0/16",
-    "0.0.0.0/20",
-    "0.0.0.0/24",
-    NULL,
-};
 
 extern gdImagePtr image;
 extern int debug;
@@ -36,20 +33,46 @@ extern struct bb cidr_to_bbox(const char *);
 extern int colors[];
 extern int num_colors;
 
+/*
+ * Show how big various blocks are
+ */
 static void
 legend_samples(void)
 {
+    char *sample_cidr[] = {
+	"0.0.0.0/8",
+	"0.0.0.0/12",
+	"0.0.0.0/16",
+	"0.0.0.0/20",
+	"0.0.0.0/24",
+	NULL,
+    };
     unsigned int i = 0;
+    int samplebox_ctr_x = legend_bb.xmin + 256;
+    int samplebox_ctr_y = 2048 + 128;
     while (sample_cidr[i]) {
 	struct bb box;
+	struct bb tbox;
+	char tstr[10];
+	int hw;
+	int hh;
 	box = cidr_to_bbox(sample_cidr[i]);
-	box.xmin += legend_title_bb.xmin;
-	box.xmax += legend_title_bb.xmin;
-	box.ymin += 2048 + (i * 275);
-	box.ymax += 2048 + (i * 275);
+	hw = BOX_WIDTH(box)/2;
+	hh = BOX_HEIGHT(box)/2;
+	box.xmin += samplebox_ctr_x - hw;
+	box.xmax += samplebox_ctr_x - hw;
+	box.ymin += samplebox_ctr_y - hh;
+	box.ymax += samplebox_ctr_y - hh;
 	gdImageFilledRectangle(image,
 	    box.xmin, box.ymin, box.xmax, box.ymax,
 	    colors[0]);
+	tbox.xmin = samplebox_ctr_x + 128;
+	tbox.xmax = tbox.xmin + 256;
+	tbox.ymin = ((box.ymin+box.ymax)/2) - 30;
+	tbox.ymax = ((box.ymin+box.ymax)/2) + 30;
+	snprintf(tstr, 10, "= %s", strchr(sample_cidr[i], '/'));
+	annotate_text(tstr, NULL, tbox);
+	samplebox_ctr_y += MAX(hh+64, 64);
 	i++;
     }
 }
