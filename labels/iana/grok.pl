754c6b32218ffb9268fec67d93a780adffeae746
Duane Wessels <wessels@measurement-factory.com>
2007-09-27 18:04:58 +0000

    fix URL in comments

--- labels/iana/grok.pl
+++ labels/iana/grok.pl
@@ -2,7 +2,7 @@
 
 my %M = qw (Jan 1 Feb 2 Mar 3 Apr 4 May 5 Jun 6 Jul 7 Aug 8 Sep 8 Oct 10 Nov 11 Dec 12);
 
-# input is http://www.iana.org/ipaddress/ip-addresses.htm
+# input is http://www.iana.org/assignments/ipv4-address-space
 # output is the same info, but in a better format
 
 use strict;
