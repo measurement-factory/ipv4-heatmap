def3922db43bb10ea4823cadfcd363c2a08cab4a
Duane Wessels <wessels@measurement-factory.com>
2007-09-27 18:06:33 +0000

    adding cron job to update iana-labels

---
+++ labels/iana/cron.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+cd `dirname $0`
+
+fetch http://www.iana.org/assignments/ipv4-address-space
+perl grok.pl < ipv4-address-space > iana-labels.txt
+yymmdd=`date +%Y-%m-%d`
+svn -q -m "version $yymmdd" commit ipv4-address-space iana-labels.txt
