7f644fc01f44cac98c72b49f71f0b59fc45773b8
Duane Wessels <wessels@measurement-factory.com>
2007-09-28 18:02:09 +0000

    use -q option to fetch

--- labels/iana/cron.sh
+++ labels/iana/cron.sh
@@ -4,7 +4,7 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 export PATH
 cd `dirname $0`
 
-fetch http://www.iana.org/assignments/ipv4-address-space
+fetch -q http://www.iana.org/assignments/ipv4-address-space
 perl grok.pl < ipv4-address-space > iana-labels.txt
 yymmdd=`date +%Y-%m-%d`
 svn -q -m "version $yymmdd" commit ipv4-address-space iana-labels.txt
