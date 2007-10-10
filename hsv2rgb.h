a78307c25b7f25122f2fd0109e5f442709af8f29
Duane Wessels <wessels@measurement-factory.com>
2007-10-10 23:42:01 +0000

    paste copyright for this file/macro from http://ilab.usc.edu/toolkit/doc-nosrc/html/README

--- hsv2rgb.h
+++ hsv2rgb.h
@@ -1,3 +1,42 @@
+/*
+ *                 ------------------------------------
+ *                 iLab Neuromorphic Vision C++ Toolkit
+ *                 ------------------------------------
+ *
+ *      The iLab Neuromorphic Vision C++ Toolkit - Copyright (C)
+ *      2001-2005 by the University of Southern California (USC) and
+ *      iLab at USC.
+ *
+ *      Major portions of the iLab Neuromorphic Vision C++ Toolkit are
+ *      protected under the U.S. patent ``Computation of Intrinsic
+ *      Perceptual Saliency in Visual Environments, and Applications''
+ *      by Christof Koch and Laurent Itti, California Institute of
+ *      Technology, 2001 (patent pending; application number
+ *      09/912,225 filed July 23, 2001; see
+ *      http://pair.uspto.gov/cgi-bin/final/home.pl for current
+ *      status)
+ *
+ *      This file is part of the iLab Neuromorphic Vision C++ Toolkit.
+ *
+ *      The iLab Neuromorphic Vision C++ Toolkit is free software; you
+ *      can redistribute it and/or modify it under the terms of the
+ *      GNU General Public License as published by the Free Software
+ *      Foundation; either version 2 of the License, or (at your
+ *      option) any later version.
+ *
+ *      The iLab Neuromorphic Vision C++ Toolkit is distributed in the
+ *      hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ *      even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *      PARTICULAR PURPOSE.  See the GNU General Public License for
+ *      more details.
+ *
+ *      You should have received a copy of the GNU General Public
+ *      License along with the iLab Neuromorphic Vision C++ Toolkit
+ *      (see the top-level file named COPYING); if not, write to the
+ *      Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *      Boston, MA 02111-1307 USA.
+ */
+
 // ######################################################################
 //T.Nathan Mundhenk
 // mundhenk @ usc.edu
