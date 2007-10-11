73c8c92e6291eba14f27adc0ebcb531c2a0cd55d
Duane Wessels <wessels@measurement-factory.com>
2007-10-11 16:19:01 +0000

    a script for making release tarballs

---
+++ mk-release.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+MANIFEST="
+LICENSE
+Makefile
+annotate.c
+bbox.c
+bbox.h
+hilbert.c
+hsv2rgb.h
+ipv4-heatmap.1
+ipv4-heatmap.c
+legend.c
+shade.c
+text.c
+labels/iana/iana-labels.txt
+labels/iana/ipv4-address-space
+labels/iana/reserved
+labels/iana/rfc1918.shades
+"
+
+if svn diff | grep . ; then
+	echo "bailing due to uncommitted changes"
+	exit 1
+fi
+
+YYMMDD=`date +%Y%m%d`
+mkdir ipv4-heatmap-$YYMMDD
+
+ls $MANIFEST | cpio -pdv ipv4-heatmap-$YYMMDD
+sed -i bak -e "s/^#undef RELEASE_VER/#define RELEASE_VER \"$YYMMDD\"/" \
+	ipv4-heatmap-$YYMMDD/ipv4-heatmap.c
+(cd ipv4-heatmap-$YYMMDD; chmod 644 $MANIFEST)
+tar czf /tmp/ipv4-heatmap-$YYMMDD.tar.gz ipv4-heatmap-$YYMMDD
+tar tzvf /tmp/ipv4-heatmap-$YYMMDD.tar.gz 
