6663823ec67a39c789f6402387575d07202c4dd5
Duane Wessels <wessels@measurement-factory.com>
2011-04-05 20:25:30 +0000

    parse the new file format (XML)

--- labels/iana/grok.pl
+++ labels/iana/grok.pl
@@ -6,6 +6,10 @@
 use strict;
 use warnings;
 use Net::CIDR::Lite;
+use XML::Simple;
+use Data::Dumper;
+
+my $XML = XMLin("-");
 
 # if $when is specified, then blocks assigned after $when
 # will be changed to "IANA - Reserved".  Format of $when
@@ -16,31 +20,22 @@ my $when = shift;
 my $cidr = Net::CIDR::Lite->new;
 my $last_desc = undef;
 
-while (<>) {
-        chomp;
-	my $block;
-	my $month;
-	my $year;
-	my $desc;
-	if (/^(\d\d\d)\/8\s+(.*)\s+(\d\d\d\d)-(\d\d)/) {
-		$block = $1;
-		$desc = $2;
-		$year = $3;
-		$month = $4;
-	} elsif (/^(\d\d\d)\/8\s+(.*)\s+UNALLOCATED/) {
-		$block = $1;
-		$desc = $2;
-		$desc =~ s/\s+$//g;
-		$desc .= " - Unallocated";
-		$year = (gmtime(time))[5];
-		$month = (gmtime(time))[4];
-	} else {
-		next;
-	}
-
-	my $sdate = $year * 10000 + $month * 100;
+sub clean_desc {
+	my $desc = shift;
+	my $sdate = shift;
+	my $block = shift;
+	my $status = shift;
 
 	# Clean up descriptions
+
+	if ('Multicast' eq $desc) {
+		1;
+	} elsif ('RESERVED' eq $status) {
+		$desc = 'Reserved';
+	} elsif ('UNALLOCATED' eq $status) {
+		$desc = 'Unallocated';
+	}
+
 	$desc =~ s/\s+$//g;
 	$desc =~ s/\s+\(.*\)$//;
 	$desc =~ s/\s+See \[.*$//;
@@ -69,25 +64,53 @@ while (<>) {
 	$desc =~ s/US Postal Service/USPS/;
 
 	#$desc = "Legacy ($1)" if ($desc =~ /^Administered by (\w+)$/);
-	$desc = "Various Registries" if ($desc =~ /^Administered by (\w+)$/ && /LEGACY/);
+	$desc = "Various Registries" if ($desc =~ /^Administered by (\w+)$/ && $status =~ /LEGACY/);
 
 	$desc = 'Reserved' if ($when && $sdate > $when);
 
-	# try to differentiate "Reserved" from "Unallocated" blocks
-	# more ugh
-	if ($desc eq 'Reserved') {
-		if ($block == 0) {
-			1;
-		} elsif ($block == 14) {
-			1;
-		} elsif ($block == 127) {
-			1;
-		} elsif ($block >= 240) {
-			1;
-		} else {
-			$desc = 'Unallocated';
-		}
-	}
+#	# try to differentiate "Reserved" from "Unallocated" blocks
+#	# more ugh
+#	if ($desc eq 'Reserved') {
+#		if ($block == 0) {
+#			1;
+#		} elsif ($block == 14) {
+#			1;
+#		} elsif ($block == 127) {
+#			1;
+#		} elsif ($block >= 240) {
+#			1;
+#		} else {
+#			$desc = 'Unallocated';
+#		}
+#	}
+	$desc;
+}
+
+#$VAR1 = {
+#          'xref' => {
+#                    'data' => '2',
+#                    'type' => 'note'
+#                  },
+#          'designation' => 'IANA - Local Identification',
+#          'status' => 'RESERVED',
+#          'date' => '1981-09',
+#          'prefix' => '000/8'
+#        };
+
+
+foreach my $rec (@{$XML->{record}}) {
+#print STDERR Dumper($rec);
+	$rec->{date} = "1970-01" unless defined $rec->{date};
+	my $block = $1 if $rec->{prefix} =~ /(\d+)\/8/;
+	my $month = $1 if $rec->{date}	 =~ /\d+-(\d+)/;
+	my $year = $1 if $rec->{date}	 =~ /(\d+)-\d+/;
+	my $desc = $rec->{designation};
+	my $status = $rec->{status};
+
+#print STDERR "$block $month $year $desc\n";
+
+	my $sdate = $year * 10000 + $month * 100;
+	$desc = clean_desc($desc, $sdate, $block, $status);
 
 	my $prefix = sprintf "%d.0.0.0/8", $block;
 	if ($last_desc && $desc ne $last_desc) {
@@ -101,6 +124,7 @@ while (<>) {
 	$cidr->add($prefix);
 	$last_desc = $desc;
 }
-		foreach my $prefix ($cidr->list) {
-			printf "%s\t%s\n", $prefix, $last_desc;
-		}
+
+foreach my $prefix ($cidr->list) {
+	printf "%s\t%s\n", $prefix, $last_desc;
+}
