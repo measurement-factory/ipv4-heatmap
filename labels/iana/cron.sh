f1e3784f415658b78dc00bfef2984c17dfa20b96
Duane Wessels <wessels@measurement-factory.com>
2018-02-14 16:59:05 +0000

    update labels

--- labels/iana/cron.sh
+++ labels/iana/cron.sh
@@ -4,7 +4,10 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 export PATH
 cd `dirname $0`
 
-fetch -q http://www.iana.org/assignments/ipv4-address-space
+wget \
+	--no-check-certificate \
+	-O ipv4-address-space \
+	https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.xml
 perl grok.pl < ipv4-address-space > iana-labels.txt
 yymmdd=`date +%Y-%m-%d`
 svn -q -m "version $yymmdd" commit ipv4-address-space iana-labels.txt
